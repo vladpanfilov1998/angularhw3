0192000694047bd9e727ab1b637aa0a997d7d6b1
vladpanfilov1998 <145s@ukr.net>
2022-05-23 00:58:54 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import {AppRoutingModule} from "./app-routing.module";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
